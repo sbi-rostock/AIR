9a88ff10e2855d95606e3a3def82f8da04c2a6d6
MattiHoch <37984689+MattiHoch@users.noreply.github.com>
2020-03-15 20:25:06 +0100

    15032020

--- PlugIns/UserDetails.js
+++ PlugIns/UserDetails.js
@@ -1,5 +1,5 @@
 (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
-var css = ".UserDetails-container {\n  padding: 10px;\n  background-color: #ffffff;\n}\n"; (require("browserify-css").createStyle(css, { "href": "__tmp_minerva_plugin\\css\\styles.css" }, { "insertAt": "bottom" })); module.exports = css;
+var css = ".UserAccount-container {\n  padding: 10px;\n  background-color: #ffffff;\n}\n"; (require("browserify-css").createStyle(css, { "href": "__tmp_minerva_plugin\\css\\styles.css" }, { "insertAt": "bottom" })); module.exports = css;
 },{"browserify-css":3}],2:[function(require,module,exports){
 "use strict";
 
@@ -21,7 +21,7 @@ require("core-js/modules/es.string.split");
 
 require('../css/styles.css');
 
-var pluginName = 'UserDetails';
+var pluginName = 'UserAccount';
 var pluginVersion = '0.9.0';
 var minervaProxyServer = 'https://minerva-dev.lcsb.uni.lu/minerva-proxy/';
 var globals = {
