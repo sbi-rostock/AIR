f9f3af2502a02fafde5e4c0b7911231532908721
Matti Hoch <37984689+MattiHoch@users.noreply.github.com>
2023-11-05 16:24:15 +0100

    Update fetchdata.js

--- BH2023_Server/fetchdata.js
+++ BH2023_Server/fetchdata.js
@@ -99,7 +99,7 @@ function getDataFromServer(request, data = {}, type = "GET", datatype = "text", 
     return new Promise((resolve, reject) => {
       $.ajax({
         type: type,
-        url: SBI_SERVER + project_hash + "/" + request,
+        url: SBI_SERVER + project_hash + "/bhs_" + request,
         contentType: contentType,
         dataType: datatype,
         data: data,
