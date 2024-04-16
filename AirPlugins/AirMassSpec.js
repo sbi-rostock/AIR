ce2bb06452a7a2558aa687ad400d002f437e5967
Matti Hoch <37984689+MattiHoch@users.noreply.github.com>
2024-04-16 21:04:36 +0200

    Update AirMassSpec.js

--- AirPlugins/AirMassSpec.js
+++ AirPlugins/AirMassSpec.js
@@ -499,13 +499,12 @@ async function AirMassSpec() {
         globals.massspec.data_chart.update();
     });
 
-    $('.air_btn_info[data-toggle="popover"]').popover({html:true})
-
     $('#air_btn_info_html[data-toggle="popover"]').popover({
         html:true,
         container: "#air_btn_info_html"
     })
 
+    $('.air_btn_info[data-toggle="popover"]').popover({html:true})
 
     $('a[data-toggle="popover"]').popover()
 
